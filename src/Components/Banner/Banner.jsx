17c32e3931fdb2948ad50512bb10c17ef040163f
Usaid ok <usaidokdev@gmail.com>
2023-12-24 07:12:04 -0800

    Feature Completed

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './Banner.css'
 function Banner() {
   return (
-    <div>
+    <>
+      <div className='banner'>
         <div className="content">
           <h2>Share Your Ideas and Thoughts 💯!!</h2>
           <p>Connect with a community of like-minded individuals</p>
@@ -10,7 +11,46 @@ function Banner() {
             <button type='button' className='getstarted'><span></span>GetStarted</button>
           </div>
         </div>
-    </div>
+      </div>
+
+      <div className="feature">
+        <div className="container">
+          <div className="row d-flex justify-content-center">
+            <h2 className='mt-4'>Feature</h2>
+          </div>
+          <div className="feature-para">
+          <p className='d-flex justify-content-center'>Analytics insights</p>
+          </div>
+          <div className="row">
+            <div className="col-12 col-md-6 col-lg-6">
+              <div className="feature-box">
+                <h4 className='text-white ml-3 pt-2'>Idea Sharing</h4>
+                <p className='text-white ml-3 mt-2'>Share your ideas and thoughts others</p>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-6">
+              <div className="feature-box">
+              <h4 className='text-white ml-3 pt-2'>Collaboration</h4>
+              <p className='text-white ml-3 mt-2'>Collaboration with others to refine and develop your ideas</p>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-6">
+              <div className="feature-box">
+              <h4 className='text-white ml-3 pt-2'>Feedback</h4>
+              <p className='text-white ml-3 mt-2'>Receive feedback and suggestions from the community</p>
+              </div>
+            </div>
+            <div className="col-12 col-md-6 col-lg-6">
+              <div className="feature-box">
+              <h4 className='text-white ml-3 pt-2'>Inspiration</h4>
+              <p className='text-white ml-3 mt-2'>Get inspired by exploring ideas and thoughts shared by others</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+
   )
 }
 
