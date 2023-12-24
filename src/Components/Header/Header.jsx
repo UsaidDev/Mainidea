7c76416fd4a834a2184d28b3a51ba09e7b2c24be
Usaid ok <usaidokdev@gmail.com>
2023-12-24 00:48:09 -0800

    Warning Solved

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -21,25 +21,25 @@ function Header() {
       <nav>
         <ul className='sidebar'>
           <li onClick={hideSidebar}>
-            <a href="#">
+            <a href="/#">
               <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26">
                 <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/>
               </svg>
             </a>
           </li>
-          <li><a href="#">Share</a></li>
-          <li><a href="#">Discover</a></li>
-          <li><a href="#">Connect</a></li>
-          <li><a href="#">Login</a></li>
+          <li><a href="/#">Share</a></li>
+          <li><a href="/#">Discover</a></li>
+          <li><a href="/#">Connect</a></li>
+          <li><a href="/#">Login</a></li>
         </ul>
         <ul>
-          <li><a href="#">MainIdea</a></li>
-          <li className='hideOnMobile'><a href="#">Share</a></li>
-          <li className='hideOnMobile'><a href="#">Discover</a></li>
-          <li className='hideOnMobile'><a href="#">Connect</a></li>
-          <li className='hideOnMobile'><a href="#">Login</a></li>
+          <li><a href="/#">MainIdea</a></li>
+          <li className='hideOnMobile'><a href="/#">Share</a></li>
+          <li className='hideOnMobile'><a href="/#">Discover</a></li>
+          <li className='hideOnMobile'><a href="/#">Connect</a></li>
+          <li className='hideOnMobile'><a href="/#">Login</a></li>
           <li className='menu-button' onClick={showSidebar}>
-            <a href="#">
+            <a href="/#">
               <svg xmlns="http://www.w3.org/2000/svg" height="26" viewBox="0 -960 960 960" width="26">
                 <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
               </svg>
--- src/Page/Navbar.jsx
+++ src/Page/Navbar.jsx
@@ -12,4 +12,4 @@ function Navbar() {
     )
 }
 
-export default Navbar
+export default Navbar;
