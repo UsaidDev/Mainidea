1557d6cdc7f833e99dc43e498207b963af506e47
Usaid ok <usaidokdev@gmail.com>
2023-12-29 06:36:13 -0800

    Datas Add Completed

--- src/Components/Writer/Writer.jsx
+++ src/Components/Writer/Writer.jsx
@@ -5,19 +5,36 @@ import { AuthContext } from '../../store/FirebaseContext'
 function Writer() {
   const { user } = useContext(AuthContext)
   const [datas, Setdatas] = useState('');
+  const [todo, Settodo] = useState([])
+  const AddList = () => {
+    Settodo([...todo, datas])
+    Setdatas('')
+  }
+
   return (
     <>
-      <div className="writer">
-        <h5>Welcome {user ? user.displayName : 'Login'}</h5>
-      </div>
-      <div className='outer'>
-        <div className="writing-page">
-          <input type="text" placeholder='Write Your ideas' value={datas} onSubmit={((e) => Setdatas(e.target.value))} />
-          <button className='write-btn'>Submit</button>
+      <div class="writer-container">
+        <div class="writer-page">
+          <h5>Welcome, <span id="userName">{user ? user.displayName : "Login"}</span></h5>
+          <textarea cols="40" rows="3" value={datas} onChange={((e) => Setdatas(e.target.value))} placeholder='Write Your Thouts..'></textarea>
+          <div class="writer-btn">
+            <div class="btn btn-success Add-btn" onClick={AddList}>Add</div>
+            <div class="btn btn-primary Delete-btn">Delete</div>
+          </div>
+          {
+            todo.map((datas) => (
+              <>
+                <div class="writer-display" key={datas}>
+                  <ul>
+                    <li>{datas}</li>
+                  </ul>
+                </div>
+              </>
+            ))
+          }
         </div>
       </div>
     </>
   )
 }
-
 export default Writer
